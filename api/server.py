28bc0425a9ea15bfeafec3eed1980386e8e32133
Spir1N <spirinchyk@gmail.com>
2025-12-04 20:43:22 +0700

    [Bot] A temporary solution for running GD through a bot

--- api/server.py
+++ api/server.py
@@ -3,15 +3,24 @@ import random
 from fastapi import FastAPI, UploadFile, File
 from pathlib import Path
 from models.yolo import yolo_detect
+from models.groundingdino.groundingdino import groundingdino_detect
 from ultralytics import YOLO
 
-MODELS_ROOT = Path(os.getenv("BOT_STORAGE_ROOT"))
+MODELS_ROOT = Path(os.getenv("BOT_MODELS_ROOT"))
 app = FastAPI()
+MODELS = ["yolo", 'gd']
+model = MODELS[1]
 
 @app.post("/process/")
 async def process_image(file: UploadFile = File(...)):
-    model = YOLO(MODELS_ROOT / "yolo" / "yolo.pt")
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(model.names))]
+    model = 'gd'
     image_bytes = await file.read()
-    result = yolo_detect(image_bytes, model, colors)
+    if model == "yolo":
+        model = YOLO(MODELS_ROOT / "yolo" / "yolo.pt")
+        colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(model.names))]
+        result = yolo_detect(image_bytes, model, colors)
+    elif model == "gd":
+        description = "coach"
+        result = groundingdino_detect(image_bytes, description)
+    
     return {"result": result.hex()}
